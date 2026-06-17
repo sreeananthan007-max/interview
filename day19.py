954c94315d978d98f0a5239b8d61c5b905f0289e
Sree Pathmanabhan <sreeananthan007@gmail.com>
2026-06-17 12:37:31 +0530

    Update day19.py

--- day19.py
+++ day19.py
@@ -39,28 +39,28 @@
 #         print("invalid choice")
 
 #--------------------------------------------------------------------------------------------------------------------------------
-class hospital:
-    def __init__(self,name,age,fee):
-        self.name=name
-        self.age=age
-        self.fee=fee
-    def bill(self):
-        if self.age>=60:
-            b=self.fee-(self.fee*0.20)
-        else:
-            b=self.fee
-        return b
-    def dis(self):
-        print("name=",self.name)
-        print("age=",self.age)
-        print("fee=",self.fee)
-        print("final bill=",self.bill())
-    def __del__(self):
-        print("patient record removal")
-name=input("enter name")
-age=int(input("enter age"))
-fee=int(input("enter fee"))
-p=hospital(name,age,fee)
-print("--------------------------------------------patient details-----------------------------------------------------------")
-p.dis()
-del p
+# class hospital:
+#     def __init__(self,name,age,fee):
+#         self.name=name
+#         self.age=age
+#         self.fee=fee
+#     def bill(self):
+#         if self.age>=60:
+#             b=self.fee-(self.fee*0.20)
+#         else:
+#             b=self.fee
+#         return b
+#     def dis(self):
+#         print("name=",self.name)
+#         print("age=",self.age)
+#         print("fee=",self.fee)
+#         print("final bill=",self.bill())
+#     def __del__(self):
+#         print("patient record removal")
+# name=input("enter name")
+# age=int(input("enter age"))
+# fee=int(input("enter fee"))
+# p=hospital(name,age,fee)
+# print("--------------------------------------------patient details-----------------------------------------------------------")
+# p.dis()
+# del p
